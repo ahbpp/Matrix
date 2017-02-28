8ac85023bb8a0d928bc98f5f9c8552aac58219a0
ahbpp <alexeykarpov@MacBook-Air-Alexey.local>
2017-02-28 22:52:08 +0300

    Begin0

--- Matrix/main.cpp
+++ Matrix/main.cpp
@@ -6,10 +6,4 @@
 //  Copyright © 2017 Alexey Karpov. All rights reserved.
 //
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+#include "main.hpp"
